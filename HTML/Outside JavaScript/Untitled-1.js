b351d3ba3bb8680dd29b26c6bb1ca9f8bbfca736
Jackson Ja <77729046+2937703346@users.noreply.github.com>
2021-08-20 20:40:49 +0800

    Add files via upload

---
+++ HTML/Outside JavaScript/Untitled-1.js
@@ -0,0 +1,14 @@
+/*var newvar=new Date()
+document.write(newvar.getDay+"/"+newvar.getMonth+"/"+newvar.getFullYear)
+*/
+
+    
+    var good = new Date()
+    console.log(good.getMonth()+1+" "+good.getDay()+' '+good.getFullYear()+" "+good.getHours()+' '+good.getMinutes()+" "+good.getSeconds())
+function QQ(){
+    var a=document.getElementById('1').value;
+    if (a>10){
+        alert(a)
+    }
+    else {alert('10')}
+}
